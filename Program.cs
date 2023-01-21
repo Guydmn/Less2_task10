ebadb817e36cb08c668083804319375d12d78f3c
Roman Guydmn <romanguydmn@gmail.com>
2023-01-21 08:00:03 +0400

    Задача10 Семинара2

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int x = (a%100) / 10;
+
+if (a > 99 && a < 1000)
+    Console.WriteLine($"Вторая цифра вашего числа {x}");
+else 
+    Console.WriteLine("введите трехзначное число");  
